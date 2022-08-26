6b4a21880e56672b584f5c028cca53448ef9b67f
Diego Aquino <diegol.aquino@gmail.com>
2022-08-25 21:53:56 -0300

    Add project files.

---
+++ Domain/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Base;
+
+namespace Domain.Models
+{
+    public class User : Entity
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime Birth { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
---
+++ Shared/Base/Entity.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Base
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            ID = Guid.NewGuid();
+        }
+
+        public Entity(Guid id)
+        {
+            ID = id;
+        }
+
+        public Guid ID { get; set; }
+    }
+}
